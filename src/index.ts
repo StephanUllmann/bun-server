c8c7ebf20259a835a0b4878cc93e990253ae0882
Stephan Ullmann <stephan.ullmann@posteo.de>
2025-06-01 16:26:43 +0200

    yolo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,38 @@
-import { Elysia } from "elysia";
+import { $ } from 'bun';
+import { Elysia, t } from 'elysia';
+import { Webhooks } from '@octokit/webhooks';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const webhooks = new Webhooks({
+  secret: process.env.WEBHOOK_SECRET!,
+});
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+const port = process.env.PORT ?? 3000;
+
+const app = new Elysia()
+  .get('/', { msg: 'Running' })
+  .post(
+    '/webhook',
+    async ({ request, headers, set }) => {
+      console.log('RUNNING');
+      const signature = headers['x-hub-signature-256'];
+      console.log(signature);
+      const payload = await request.text();
+      const isLegit = await webhooks.verify(payload, signature);
+      console.log({ isLegit });
+      set.status = 202;
+      new Response('Accepted');
+      return { msg: 'running POST /webhook' };
+    },
+    {
+      headers: t.Object({
+        'x-hub-signature-256': t.String(),
+      }),
+      error({ error }) {
+        console.log(error);
+        return 'happens';
+      },
+    }
+  )
+  .listen(port);
+
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
